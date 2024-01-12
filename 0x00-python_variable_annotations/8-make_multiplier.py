f7a4fd39302c8f035c9202bb68ac96c60f491cf1
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-01-12 07:09:20 +0000

    add task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,11 +3,6 @@
 from typing import Callable
 
 
-def multiple(num: float) -> float:
-    """Gives the power 2 of a number"""
-    return num * num
-
-
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Gives the sum of a list of floats and integers"""
-    return multiple
+    return lambda x: x * multiplier
