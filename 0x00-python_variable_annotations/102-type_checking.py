bbf9decab4b00f05a4e2f56045f4e5462a1c33f0
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-01-12 07:19:50 +0000

    add task 100,101,102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Define to_kv function"""
+"""Define zoom_array function"""
 from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """test"""
+    """Creates multiple copies of items in a tuple"""
     zoomed_in: List = [item for item in lst for i in range(int(factor))]
     return zoomed_in
 
