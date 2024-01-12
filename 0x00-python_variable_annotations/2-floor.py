ddef76a1cdf3c302df6189ae6d4fb8cce6116e46
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-01-12 06:48:22 +0000

    add task 2,3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Define floor function"""
+
+
+def floor(n: float) -> int:
+    """gives the floor of an float"""
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Define to_str function"""
+
+
+def to_str(n: float) -> str:
+    """convert float to a string"""
+    return str(n)
